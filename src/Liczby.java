7281c3036f074bd414b62172b3e7c062dad6286e
Kasia RN <rudyknowak@gmail.com>
2018-01-23 17:46:52 +0100

    do sprawdzenia

---
+++ src/Liczby.java
@@ -0,0 +1,13 @@
+public class Liczby {
+    public static void main(String[] args) {
+        int [] liczba = new int[5];
+        liczba[0]=1;
+        liczba[1]=2;
+        liczba[2]=3;
+        liczba[3]=4;
+        liczba[4]=5;
+
+        int suma= liczba[0]+liczba[2]+liczba[4];
+        System.out.println(suma);
+    }
+}
